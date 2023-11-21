b31071eea91fd41eeb427eb167aba07b7539b752
Surafels <ssurafel4@gmail.com>
2023-11-21 17:19:21 +0300

    Add private keyword

--- person.rb
+++ person.rb
@@ -1,25 +1,26 @@
 class Person
-  def initialize(id, age, parent_permission: true, name: 'Unknown')
-    @id = id
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(age, parent_permission: true, name: 'Unknown')
+    @id = Random.rand(1...1000)
     @age = age
     @name = name
     @parent_permission = parent_permission
   end
 
-  attr_accessor :name, :age
-  attr_reader :id
-
-
   def can_use_services?
     of_age? || @parent_permission
   end
+
+  private
 
   def of_age?
     @age >= 18
   end
 end
 
-person = Person.new(1, 30)
+person = Person.new(30)
 puts person.name
 puts person.age
 
@@ -27,6 +28,4 @@ person.name = 'surafel'
 person.age = 27
 puts person.name
 puts person.age
-
-puts person.of_age?
 puts person.can_use_services?
