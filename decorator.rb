a68d20759a36ab145d01ddfc7f33f6c402aa6c1c
Surafels <ssurafel4@gmail.com>
2023-11-22 15:42:56 +0300

    Add decorator.rb

--- decorator.rb
+++ decorator.rb
@@ -1,4 +1,11 @@
 require_relative 'nameable'
-class Decorator
-  def 
+class BaseDecorator < Namable
+  def initialize(namable)
+    super()
+    @namable = namable
+  end
+
+  def correct_name
+    @namable.correct_name
+  end
 end
