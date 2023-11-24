e606d14971bea46edd9f25587b8ec5dd81d68aad
Surafels <ssurafel4@gmail.com>
2023-11-24 11:41:18 +0300

    Add Book class

--- student.rb
+++ student.rb
@@ -6,7 +6,7 @@ class Student < Person
   def initialize(age, classroom, parent_permission: true, name: 'Unknown')
     super(age, parent_permission: parent_permission, name: name)
     @classroom = classroom
-    classroom.add_classroom(self) if classroom
+    classroom&.add_classroom(self)
   end
 
   def play_hooky
