f04596065d27933f0e924d162c774faf3267c8ce
Surafels <ssurafel4@gmail.com>
2023-11-24 12:09:52 +0300

    Update book.rb

--- book.rb
+++ book.rb
@@ -1,8 +1,14 @@
 class Book
-  attr_accessor :title, :author
+  attr_reader :title, :author
+  attr_accessor :rentals
 
   def initialize(title, author)
     @title = title
     @author = author
+    @rentals = []
+  end
+
+  def persons
+    rentals.map(&:person)
   end
 end
